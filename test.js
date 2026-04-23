dafd0fb51a42205af58aaeada6715bfc0dd7da2f
Danpro2626 <118908308+Danpro2626@users.noreply.github.com>
2026-04-23 22:18:34 +0000

    fix: corrijo el test para volver al verde

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { sumar } = require('./app');
 
 test('sumar 2 + 3 debe dar 5', () => {
-  expect(sumar(2, 3)).toBe(99);
+  expect(sumar(2, 3)).toBe(5);
 });
 
 test('sumar 0 + 0 debe dar 0', () => {
