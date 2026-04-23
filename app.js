3d3f3cbb6ba2d23d8131d70cf5f225c7b706566a
Danpro2626 <118908308+Danpro2626@users.noreply.github.com>
2026-04-23 21:39:39 +0000

    feat: agrego app, tests y pipeline CI

---
+++ app.js
@@ -0,0 +1,15 @@
+// Función principal: suma dos números
+function sumar(a, b) {
+  return a + b;
+}
+
+// Servidor web simple
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola DevOps! El resultado de 2+3 es: ' + sumar(2, 3));
+});
+
+server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+module.exports = { sumar };
