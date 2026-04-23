ea0091bb986d1be4fab909ee99553826070b0da5
Danpro2626 <118908308+Danpro2626@users.noreply.github.com>
2026-04-23 21:46:26 +0000

    fix: evitar que el servidor bloquee los tests

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ function sumar(a, b) {
   return a + b;
 }
 
-// Servidor web simple
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hola DevOps! El resultado de 2+3 es: ' + sumar(2, 3));
-});
-
-server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+// Solo iniciamos el servidor si ejecutamos el archivo directamente
+if (require.main === module) {
+  const http = require('http');
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hola DevOps! El resultado de 2+3 es: ' + sumar(2, 3));
+  });
+  server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+}
 
 module.exports = { sumar };
