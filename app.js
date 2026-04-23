ab01a2cf0a2857484d5979965b69232000938eb6
Danpro2626 <118908308+Danpro2626@users.noreply.github.com>
2026-04-23 22:00:06 +0000

    fix: app.js sin servidor HTTP para que Jest no quede colgado

--- app.js
+++ app.js
@@ -2,13 +2,4 @@ function sumar(a, b) {
   return a + b;
 }
 
-if (require.main === module) {
-  const http = require('http');
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola DevOps! El resultado de 2+3 es: ' + sumar(2, 3));
-  });
-  server.listen(3000);
-}
-
 module.exports = { sumar };
